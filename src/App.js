19c8547949e11699c812efcb8ea45bd387179819
unkwon <bruno_shenn@hotmail.com>
2018-02-07 09:32:38 -0200

    save course three

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+const App = () => {
+	return (
+		<View style={{ flex: 1, justifyContent: 'center'}}>
+			<Text>Oi oi oi oi</Text>
+			<Text>Oi oi oi oi</Text>
+		</View>
+	);
+};
+
+export default App;
