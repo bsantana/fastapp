c59955e332d172027d5cd4108a14e5a84a999fe5
unkwon <bruno_shenn@hotmail.com>
2018-03-03 14:01:25 -0300

    Adicionando telas de map com posição atual e assistida

--- src/Router.js
+++ src/Router.js
@@ -3,13 +3,17 @@ import { Router, Scene } from 'react-native-router-flux';
 
 import List from './components/List';
 import PostDetail from './components/PostDetail';
+import ComponentMap from './components/Map';
+import ComponentMapReload from './components/MapReload';
 
 const RouterComponent = () => {
     return(
-        <Router sceneStyle={{ paddingTop: 10 }}>
+        <Router>
             <Scene>
-                <Scene key='list' component={List} title='Lista de Posts' initial />
+                <Scene key='list' component={List} title='Lista de Posts'  />
                 <Scene key='postDetail' component={PostDetail} title='Ver Post' />
+                <Scene key='componentMap' component={ComponentMap} title='Mapa' initial />
+                <Scene key='componentMapReload' component={ComponentMapReload} title='Mapa Atualizado' />
             </Scene>
         </Router>   
     );
---
+++ src/components/Map.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+import { Actions } from 'react-native-router-flux';
+
+class GeolocationExample extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      latitude: null,
+      longitude: null,
+      error: null,
+    };
+  }
+
+  componentDidMount() {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        this.setState({
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude,
+          error: null,
+        });
+      },
+      (error) => this.setState({ error: error.message }),
+      { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 },
+    );
+  }
+
+  render() {
+    return (
+      <View style={{ flexGrow: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Latitude: {this.state.latitude}</Text>
+        <Text>Longitude: {this.state.longitude}</Text>
+        {this.state.error ? <Text>Error: {this.state.error}</Text> : null}
+        <Text style={{ marginTop: 15 }} onPress={() => Actions.componentMapReload()}>MONITORAR POSIÇÃO</Text>
+      </View>
+    );
+  }
+}
+
+export default GeolocationExample;
